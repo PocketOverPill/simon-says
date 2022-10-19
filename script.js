e790e10e27f51f1adee2bae82708ce4aaf52fe7b
André R. Pasqualotto <andre.rpasqualotto@gmail.com>
2022-10-19 17:07:57 -0300

    comecando a lógica do JS

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("OIS");
+
+var green = document.getElementById("green");
+var red = document.getElementById("red");
+var blue = document.getElementById("blue");
+var yellow = document.getElementById("yellow");
+var atual;
+
+
+function escolheCor(){
+    var random = Math.floor(Math.random()*4)
+    switch (random) {
+        case 0:
+            atual = green;
+            break;
+        case 1:
+            atual = red;
+            break;
+        case 2:
+            atual = blue;
+            break;
+        case 3:
+            atual = yellow;
+            break;      
+        default:
+            break;    }
+            console.log(atual);
+
+
+}
+
+
+
+
