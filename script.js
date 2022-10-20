a53e93b455d6b607764ca5fa61d9afcc98490715
André R. Pasqualotto <andre.rpasqualotto@gmail.com>
2022-10-19 22:42:07 -0300

    funcao escolhe cor piscando div

--- script.js
+++ script.js
@@ -12,19 +12,29 @@ function escolheCor(){
     switch (random) {
         case 0:
             atual = green;
+            atual.style.backgroundColor = "white";
+            setTimeout(() => { atual.style.backgroundColor = "green"; }, 300);
             break;
         case 1:
             atual = red;
+            atual.style.backgroundColor = "white";
+            setTimeout(() => { atual.style.backgroundColor = "red"; }, 300);
             break;
         case 2:
             atual = blue;
+            atual.style.backgroundColor = "white";
+            setTimeout(() => { atual.style.backgroundColor = "blue"; }, 300);
             break;
         case 3:
             atual = yellow;
+            atual.style.backgroundColor = "white";
+            setTimeout(() => { atual.style.backgroundColor = "yellow"; }, 300);
             break;      
         default:
             break;    }
             console.log(atual);
+
+            atual.style.backgroundColor = "white";
 
 
 }
